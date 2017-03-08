8fe6d614c6c6126a28152292894864c2c6f61973
JQ <JQ@JQ-PC>
2017-03-09 00:27:47 +0800

    made changes kay gwapo ko

--- app/User.php
+++ app/User.php
@@ -17,8 +17,10 @@ class User extends Authenticatable
     
     
     protected $table = 'user';
+    
+    
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     /**
