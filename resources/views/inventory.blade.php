b2882e0b9e7661c97a07e624b3c405fb5c71123d
ROG <ROG@172.17.2.78>
2017-03-19 00:38:41 +0800

    switching tabs and open modal

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -10,11 +10,9 @@
 
   <div class="container-fluid jobordercontent">
     <br><br>
-              <button type="button" data-toggle="modal" data-target="#rawMatsModal" class="btn btn-default">Add</button>
-              <button type="button" data-toggle="modal" data-target="#prodModal" class="btn btn-default">Add</button>
       <ul class="nav nav-pills">
-        <li class="active"><a data-toggle="pill" href="#rawmats">Raw Materials</a></li>
-        <li><a data-toggle="pill" href="#finishedprods">Finished Product</a></li>
+        <li class="active"><a href="javascript:hideTable1();">Raw Materials</a></li>
+        <li><a href="javascript:hideTable2();">Finished Product</a></li>
       </ul>
     </div>
   </div>
@@ -26,6 +24,8 @@
             
         <div class="panel panel-default">
             <div class="panel-body">
+                <button type="button" data-toggle="modal" data-target="#rawMatsModal" class="btn btn-primary">+ Add Raw Material</button>
+                <br><br>
           
               <table id="tableRawMats" class="table-hover">
               <thead>
@@ -47,7 +47,7 @@
                   <td>{{$rawMat->quantityAvailable}}</td>
                   <td>{{$rawMat->reOrderPoint}}</td>
                   <td><a href="{{ 'rawMats/'.$rawMat->rawMaterialsID.'/edit' }}"><span class ="glyphicon glyphicon-edit"</span></td>
-                  <td>help</td>
+                  <td><a href="{{ 'rawMats/'.$rawMat->rawMaterialsID.'/delete' }}"><span class ="glyphicon glyphicon-trash"</span></td>
               </tr>
               @endforeach
               </tbody>
@@ -58,10 +58,12 @@
         </div>
       </div>
 
-      <div id="finishedprods" class="tab-pane fade">
+      <div id="finishedprods" class="tab-pane fade hidden">
         <div class="container finishedproducttable">
         <div class="panel panel-default">
           <div class="panel-body">
+              <button type="button" data-toggle="modal" data-target="#prodModal" class="btn btn-primary">+ Add Product</button>
+              <br><br>
             <table id="tableFinishedProducts" class="table-hover">
               <thead>
                 <tr>
@@ -81,7 +83,7 @@
                   <td>{{$product->quantityAvailable}}</td>
                   <td>{{$product->unitPrice}}</td>
                   <td><a href="{{ 'prod/'.$product->productID.'/edit' }}"><span class ="glyphicon glyphicon-edit"</span></td>
-                  <td>help</td>
+                  <td><a href="{{ 'prod/'.$product->productID.'/delete' }}"><span class ="glyphicon glyphicon-trash"</span></td>
               </tr>
               @endforeach
               </tbody>
@@ -91,10 +93,8 @@
           
         </div>
       </div>
-                <div id="rawMatsModal" class="modal fade" role="dialog">
-                
+                <div id="rawMatsModal" class="modal fade" role="dialog">                
                   <div class="modal-dialog">
-
                     <!-- Modal content-->
                     <div class="modal-content">
                         <form action="{{ url('/inventory/rawmats/create') }}" method="POST">
@@ -117,18 +117,16 @@
                             </div>
                         </div>
                                  <div class="modal-footer">
-                                    <button type="submit" class="btn btn-default" id ="createbutton"> Add Raw Material</button>
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-primary" id ="createbutton"> Add Raw Material</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                                 </div>
                          </form>
                     </div>
-
                   </div>
                 </div>
-                <div id="prodModal" class="modal fade" role="dialog">
-                
+    
+                <div id="prodModal" class="modal fade" role="dialog">                
                   <div class="modal-dialog">
-
                     <!-- Modal content-->
                     <div class="modal-content">
                         <form action="{{ url('/inventory/prod/create') }}" method="POST">
@@ -148,14 +146,73 @@
                             </div>
                         </div>
                                  <div class="modal-footer">
-                                    <button type="submit" class="btn btn-default" id ="createbutton"> Add Product</button>
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-primary" id ="createbutton"> Add Product</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                                 </div>
                          </form>
                     </div>
-
+                  </div>                    
+                </div>
+    
+                <div id="rawMatsModalEdit" class="modal fade" role="dialog">                
+                  <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <form action="{{ url('/inventory/rawmats/update') }}" method="POST">
+                         {{ csrf_field() }}
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Edit Raw Material</h4>
+                            </div>
+                        <div class="modal-body">
+                            <div class ="form-group">
+                               Name of Material:<br>
+                               <input type ="text" name ="mname" class ="form-control">
+                               Description:<br>
+                               <input type ="text" name ="description" class ="form-control">
+                               Reorder Quantity:<br>
+                               <input type ="text" name ="reorderpt" class ="form-control">
+                               
+                            </div>
+                        </div>
+                                 <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary" id ="createbutton">Update Raw Material</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                                </div>
+                         </form>
+                    </div>
                   </div>
                 </div>
+                
+                <div id="prodModalEdit" class="modal fade" role="dialog">                
+                  <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <form action="{{ url('/inventory/prod/update') }}" method="POST">
+                         {{ csrf_field() }}
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Edit Product</h4>
+                            </div>
+                        <div class="modal-body">
+                            <div class ="form-group">
+                               Name of Product:<br>
+                               <input type ="text" name ="pname" class ="form-control">
+                               Description:<br>
+                               <input type ="text" name ="description" class ="form-control">
+                               Price:<br>
+                               <input type ="text" name ="price" class ="form-control">
+                            </div>
+                        </div>
+                                 <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary" id ="createbutton">Update Product</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                                </div>
+                         </form>
+                    </div>
+                  </div>                    
+                </div>
+                
 <script>
 $(document).ready(function(){
     $('#tableRawMats').dataTable( {
@@ -175,8 +232,23 @@ $('#tableFinishedProducts').dataTable( {
                 "sPrevious": "<i style='position: relative; top:2px;' class='fa fa-angle-left'></i>"
                   }
               }
-    });    
+    });
+    
 });
+
+function hideTable1(){
+        $('#finishedprods').addClass('hidden');
+        $('#rawmats').removeClass('hidden');
+        $('#finishedprods').removeClass('in active');
+        $('#rawmats').addClass(' in active');
+}
+
+function hideTable2(){
+        $('#finishedprods').removeClass('hidden');
+        $('#rawmats').addClass('hidden');
+        $('#rawmats').removeClass(' in active');
+        $('#finishedprods').addClass('in active');
+}
 
 </script>
 @endsection
